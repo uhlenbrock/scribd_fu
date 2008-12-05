8328512cbfa6d632117df9120dc0d2ab9e367bd1
Bobby Uhlenbrock <bobby@thinkbarefoot.com>
2008-12-05 15:35:26 -0500

    Implemented Scribd JS API as options hash for display_scribd

--- lib/scribd_fu_helper.rb
+++ lib/scribd_fu_helper.rb
@@ -1,5 +1,9 @@
 module ScribdFuHelper
   
+  # Available parameters for the JS API
+  # http://www.scribd.com/publisher/api/api?method_name=Javascript+API
+  ScribdJSParams = [ :height, :width, :page, :my_user_id, :search_query, :jsapi_version, :disable_related_docs, :mode, :auto_size ]
+  
   # Displays the scribd object for the attachment on the given +object+.
   #
   # If you are using Paperclip, you must also specify the +attribute+ on
@@ -12,11 +16,13 @@ module ScribdFuHelper
       scribd_id = object.scribd_id
       scribd_ak = object.scribd_access_key
     end
+    params = options[:params].collect { |k,v| "scribd_doc.addParam('#{k}','#{v}');" if ScribdJSParams.include?(k) }.compact.join("\n")
     <<-END
       <script type=\"text/javascript\" src=\"http://www.scribd.com/javascripts/view.js\"></script>
       <div id=\"embedded_flash\">#{options[:alt_text]}</div>
       <script type=\"text/javascript\">
         var scribd_doc = scribd.Document.getDoc(#{scribd_id}, '#{scribd_ak}');
+        #{params}
         scribd_doc.write(\"embedded_flash\");
       </script>
     END
