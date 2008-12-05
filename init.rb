3b0e9bd8f53bec740f6fe5f4188683ec1dea905b
Bobby Uhlenbrock <bobby@thinkbarefoot.com>
2008-12-05 18:25:24 -0500

    missing a require

--- init.rb
+++ init.rb
@@ -1,5 +1,6 @@
 require 'scribd_fu'
 require 'scribd_fu_helper'
+require 'scribd_fu_controller'
 ActiveRecord::Base.send(:include, ScribdFu)
 ActionView::Base.send(:include, ScribdFuHelper)
 ActionController::Base.send(:include, ScribdFuController)
